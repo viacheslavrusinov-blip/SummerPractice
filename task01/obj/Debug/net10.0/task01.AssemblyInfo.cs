8e59ff124cec4653d587e0a01be26b87d40cb31b
Viacheslav Rusinov <viacheslavrusinov@gmail.com>
2026-06-24 13:10:48 +0600

    fix: rename tests

--- task01/obj/Debug/net10.0/task01.AssemblyInfo.cs
+++ task01/obj/Debug/net10.0/task01.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("task01")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+86c5506fb9c5509dfde12d1e195b6d6dd0bef5c1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6bc7e9c799c3ac59b37b1e2234370e38172945dc")]
 [assembly: System.Reflection.AssemblyProductAttribute("task01")]
 [assembly: System.Reflection.AssemblyTitleAttribute("task01")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
