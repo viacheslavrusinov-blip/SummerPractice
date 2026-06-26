9d1121ae8a1aa557fd981c0b96e8caa9fafec4a4
Viacheslav Rusinov <viacheslavrusinov@gmail.com>
2026-06-26 16:46:19 +0600

    chore : init

---
+++ task01/task01.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace task01
+{
+    public static class StringExtensions
+    {
+        public static bool IsPalindrome(this string input)
+        {
+            if (string.IsNullOrEmpty(input))
+            {
+                return false;
+            }
+            string stringWithLower = input.ToLower();
+            List<char> ListChars = new List<char>();
+            foreach (char c in stringWithLower)
+            {
+                if (!char.IsPunctuation(c) && !char.IsWhiteSpace(c))
+                {
+                    ListChars.Add(c);
+                }
+            }
+            if (ListChars.Count == 0)
+            {
+                return false;
+            }
+            string stringChars = new string(ListChars.ToArray());
+            char[] reversedArray = stringChars.ToArray();
+            Array.Reverse(reversedArray);
+            string reversedString = new string(reversedArray);
+            return stringChars == reversedString;
+        }
+    }
+}
