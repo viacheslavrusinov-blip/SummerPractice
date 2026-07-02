be2126ba9ecce7f76067ad160c52d3e6e6dbfb0e
Viacheslav Rusinov <viacheslavrusinov@gmail.com>
2026-07-02 17:03:37 +0600

    chore : init

---
+++ task07/task07.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Reflection;
+using Microsoft.VisualBasic;
+namespace task07;
+[System.AttributeUsage(System.AttributeTargets.All, Inherited = false, AllowMultiple = true)]
+public sealed class DisplayNameAttribute : System.Attribute
+{
+    public string DisplayName;
+    public DisplayNameAttribute(string DisplayName)
+    {
+        this.DisplayName = DisplayName;
+    }
+    
+    
+    
+}
+
+[System.AttributeUsage(System.AttributeTargets.All, Inherited = false, AllowMultiple = true)]
+public sealed class  VersionAttribute : System.Attribute
+{
+    public VersionAttribute(int Major, int Minor)
+    {
+        this.Major = Major;
+        this.Minor = Minor;
+    }
+    public int Major;
+    public int Minor;
+
+}
+[Version(1, 0)]
+[DisplayName("Пример класса")]
+public class SampleClass
+{   [DisplayName("Тестовый метод")]
+    public void TestMethod()
+    {
+        
+    }
+    [DisplayName("Числовое свойство")]
+    public int Number {get; set;}
+
+}
+
+public static class ReflectionHelper
+{
+    public static void PrintTypeInfo(Type _type)
+    {
+        if (_type == null)
+        {
+            return;
+        }
+        var classDisplay = _type.GetCustomAttribute<DisplayNameAttribute>();
+        if (classDisplay != null)
+        {
+            Console.WriteLine($"Display Name Class: {classDisplay.DisplayName}");
+        }
+        var classVersion = _type.GetCustomAttribute<VersionAttribute>();
+        if (classVersion != null)
+        {
+            Console.WriteLine($"Class version: {$"{classVersion.Major}.{classVersion.Minor}"}");
+        }
+
+        var properties = _type.GetProperties();
+        if (properties != null)
+        {
+            foreach (var prop in properties)
+            {
+                Console.Write($"{prop.GetCustomAttribute<DisplayNameAttribute>()} - {prop.Name}");
+            }
+        }
+
+        var methods = _type.GetMethods();
+        if (methods != null)
+        {
+            foreach ( var method in methods)
+            {
+                Console.Write($"{method.GetCustomAttribute<DisplayNameAttribute>()} - {method.Name}");
+            }
+        }
+    }
+}
+
